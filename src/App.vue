a37c786cbbfc004054492993ddb6885df006c05e
sholt20 <samuelholt123@gmail.com>
2020-11-21 20:07:43 -0500

    Added modal that pops up after clicking stay on page on leave confirmation dialogue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,64 @@
 <template>
-<div id="app">
-  <nav>
-    <NavBar :name="$route.name"/>
-  </nav>
-  <main>
-    <router-view/>
-  </main>
-</div>
+    <div id="app">
+        <ConfirmLeave v-if="state.isConfirmOpen" :state="state" @close-modal="closeModal"/>
+        <nav>
+            <NavBar :name="$route.name" />
+        </nav>
+        <main>
+            <router-view />
+        </main>
+    </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import { onMounted, reactive } from 'vue';
+import NavBar from "./components/NavBar.vue";
+import ConfirmLeave from "./components/ConfirmLeave.vue"
 export default {
-  name: 'App',
-  components: {
-    NavBar
-  }
-}
+    name: "App",
+    components: {
+        NavBar,
+        ConfirmLeave
+    },
+    setup() {
+        const state = reactive({
+            isConfirmOpen: false
+        })
+
+        onMounted(() => {
+            window.addEventListener('beforeunload', e => {
+                console.log('Here')
+                handleLeave()
+                e.returnValue = 'no'
+            })
+        })
+
+        function handleLeave() {
+            state.isConfirmOpen = true
+        }
+
+        function closeModal() {
+            state.isConfirmOpen = false
+        }
+
+        return {
+            state,
+            handleLeave,
+            closeModal
+        }
+    },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Xanh+Mono&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Xanh+Mono&display=swap");
 a {
   text-decoration: none;
   color: white;
 }
 
 body {
-  font-family: 'Xanh Mono', monospace;
+  font-family: "Xanh Mono", monospace;
   font-size: 30px;
   background-color: rgb(246, 237, 250);
   margin: 0;
---
+++ src/components/ConfirmLeave.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="background" @click="closeModal">
+        <div class="confirmation-modal">
+            <span class="scolding-text">Leaving is dangerous - you are not yet prepared</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ConfirmLeave',
+    setup(props, ctx) {
+
+        function closeModal() {
+            ctx.emit('close-modal')
+        }
+
+        return {
+            closeModal
+        }
+    }
+}
+</script>
+
+<style scoped>
+.background {
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.confirmation-modal {
+    background-color: white;
+    width: 50%;
+    height: 40%;
+    z-index: 11;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 5px;
+}
+</style>

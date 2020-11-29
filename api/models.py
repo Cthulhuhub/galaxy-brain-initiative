4640db29df0268029277695f081f3bd3d6987fee
sholt20 <samuelholt123@gmail.com>
2020-11-29 14:16:14 -0500

    Work on auth

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(requrest.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect('/')
